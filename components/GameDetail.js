da96ae9150a667efb704bad14a6a1a2b800d8463
Neel Coffin <nlcoffin@free.fr>
2022-05-31 18:26:36 +0200

    :bug: fix: responsive mobile view game detail

--- components/GameDetail.js
+++ components/GameDetail.js
@@ -2,22 +2,27 @@ import {React} from "react";
 
 import styles from '../styles/Details.module.css';
 import GenericCard from "./GenericCard.js";
+import useBreakpoint from "../hooks/useBreakpoint";
 
 const GameDetail = ({game}) => {
     console.log(game)
+    const {breakPointName} = useBreakpoint()
     return (
         <GenericCard>
             <div>
-                <div className={styles['details-container']}>
-                    <img className={styles['game-detail-img']} src={game.cover}/>
-                    <ul>
+                <div className={styles[`details-container-${breakPointName}`]}>
+                    <div className={styles[`details-image-container`]}>
+                        <img className={styles['game-detail-img']} src={game.cover}/>
+                    </div>
+
+
                         <div className={styles['game-detail-title']}>
                             {JSON.stringify(game.name).replaceAll('"', '')}
                         </div>
                         <div className={styles['game-detail-score']}>
                             {JSON.stringify(game.aggregated_rating)}%
                         </div>
-                    </ul>
+
 
                 </div>
                 <div className={styles['game-detail-summary']}>
