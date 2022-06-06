343072c485d6ea9208e85827a74ef5c32f543b28
Neel Coffin <nlcoffin@free.fr>
2022-06-06 18:48:32 +0200

    :sparkles: most finished games: list of most finished games

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import styles from "../styles/TopCardsList.module.css";
 import {React} from "react";
 
 
-function Home({games, games2, cookies}) {
+function Home({games, games2, games3, cookies}) {
     const titleCat1 = 'Top 10 recent games (last 3 months)';
     const titleCat2 = 'Best Rating game on the platform';
+    const titleCat3 = 'Most Finished game on the platform';
 
     return (
         <div>
@@ -18,6 +19,12 @@ function Home({games, games2, cookies}) {
                     (games2 == null || games2.length === 0)
                         ? <span className={styles['not-enough-data']}>Not enough data to show this category.</span>
                         :  <TopCardsList games={games2} />
+                }
+                <div className={styles['title-category']}> {titleCat3} </div>
+                {
+                    (games3 == null || games3.length === 0)
+                        ? <span className={styles['not-enough-data']}>Not enough data to show this category.</span>
+                        :  <TopCardsList games={games3} />
                 }
             </div>
         </div>
@@ -31,8 +38,7 @@ export async function getStaticProps() {
         game.isIgdb = true;
     })
 
-
-    const res2 = await fetch(new URL('/games/bestRating', process.env.NEXT_PUBLIC_MAIN_API_URL))
+    const res2 = await fetch(new URL('/games/bestRating', process.env.NEXT_PUBLIC_MAIN_API_URL).toString())
     let games2 = await res2.json()
     games2 = await Promise.all(games2.map(async (g) => {
         if (g.igdb_id != null) {
@@ -46,10 +52,26 @@ export async function getStaticProps() {
             return g
         }
     }));
+    const res3 = await fetch(new URL('/games/mostFinished', process.env.NEXT_PUBLIC_MAIN_API_URL).toString())
+    let games3 = await res3.json()
+    games3 = await Promise.all(games3.map(async (g) => {
+        if (g.igdb_id != null) {
+            const url = new URL('/api/igdb/games/byId', process.env.NEXT_PUBLIC_BTB_API_URL)
+            url.searchParams.set('id', g.igdb_id)
+
+            const r = await fetch(url.toString())
+            const rcover = await r.json()
+            return {...g, cover:rcover[0].cover}
+        } else {
+            return g
+        }
+    }));
+
     return {
         props: {
             games,
             games2,
+            games3,
         },
     }
 }
