3ed6af1d403fa1d5eace77cc00cbcfb224d1417d
Galimede <mat.degand@outlook.fr>
2022-06-06 20:36:12 +0200

    :sparkles: category with playtime on the homepage

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import styles from "../styles/TopCardsList.module.css";
 import {React} from "react";
 
 
-function Home({games, games2, games3, cookies}) {
+function Home({games, games2, games3, games4, cookies}) {
     const titleCat1 = 'Top 10 recent games (last 3 months)';
     const titleCat2 = 'Best Rating game on the platform';
     const titleCat3 = 'Most Finished game on the platform';
+    const titleCat4 = 'Games with the highest playtime';
 
     return (
         <div>
@@ -26,12 +27,19 @@ function Home({games, games2, games3, cookies}) {
                         ? <span className={styles['not-enough-data']}>Not enough data to show this category.</span>
                         :  <TopCardsList games={games3} />
                 }
+
+                <div className={styles['title-category']}> {titleCat4} </div>
+                {
+                    (games4 == null || games4.length === 0)
+                        ? <span className={styles['not-enough-data']}>Not enough data to show this category.</span>
+                        :  <TopCardsList games={games4} />
+                }
             </div>
         </div>
     )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
     const res = await fetch(new URL('api/igdb/topten', process.env.NEXT_PUBLIC_BTB_API_URL))
     const games = await res.json()
     await games.forEach((game) => {
@@ -67,11 +75,27 @@ export async function getStaticProps() {
         }
     }));
 
+    const res4 = await fetch(new URL('/games/averagePlaytime', process.env.NEXT_PUBLIC_MAIN_API_URL).toString())
+    let games4 = await res4.json()
+    games4 = await Promise.all(games4.map(async (g) => {
+        if (g.igdb_id != null) {
+            const url = new URL('/api/igdb/games/byId', process.env.NEXT_PUBLIC_BTB_API_URL)
+            url.searchParams.set('id', g.igdb_id)
+
+            const r = await fetch(url.toString())
+            const rcover = await r.json()
+            return {...g, cover:rcover[0].cover}
+        } else {
+            return g
+        }
+    }));
+
     return {
         props: {
             games,
             games2,
             games3,
+            games4,
         },
     }
 }
